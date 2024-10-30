68aa9037419c794710af9110313ae606f00ecce1
Tinku778 <123795563+Tinku778@users.noreply.github.com>
2024-10-30 11:35:48 +0530

    Update tot.js

--- netlify/tot.js
+++ netlify/tot.js
@@ -15,18 +15,4 @@ const toggle = document.getElementById('darkModeToggle');
                 icon.style.color = 'white'; // Optional: Change icon color in light mode
             }
         });
-document.getElementById('contactform').addEventListener("submit", submitForm);
 
-async function submitForm(e) {
-        document.querySelector('.alert').style.display = 'block';
-        setTimeout(() => {
-            document.querySelector('.alert').style.display = 'none';
-        }, 3000);
-        
-        document.getElementById("contactform").reset();
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-const GetEleId = (id) => document.getElementById(id).value;
